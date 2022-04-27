26b39780bff1ed111c9706ba1a0071e54490d4a6
Débora Christyna <cdias.proj@gmail.com>
2022-04-26 23:53:14 -0300

    Arquivos V0 Projeto Guiado

---
+++ app.js
@@ -0,0 +1,50 @@
+//sincronizando dados de outras abas
+const read = require('readline-sync')
+const buscarLivros = require('./controllers/buscarLivros')
+const listarLivrosOrdenados = require('./controllers/listarLivrosOrdenados')
+const listaRecomendados = require('./controllers/listaRecomendados')
+const listarLivrosNaoLidos = require('./controllers/listarLivrosNaoLidos')
+
+// //teste de funcionamento
+// console.log(livros)
+// const resposta = read.question('Esta funcionando? (S/N) ').toUpperCase()
+// console.log(resposta)
+
+/*Requisitos
+ buscarLivros Deverá ser possível buscar livros pela categoria
+ listarLivrosOrdenados Deverá retornar todos os livros ordenados de forma crescente por quantidade de páginas.
+ listarRecomendados Deverá listar livros que já foram lidos e que são recomendados
+ listarLivrosNaoLidos Deverá listar livros que ainda não foram lidos */
+
+const resposta = read.question(`
+ =============== Menu ============
+ 1 - CATEGORIA: Buscar livro por Categoria
+ 2 - ORDENAR: Ordenar livros por quantidade de pagina
+ 3 - RECOMENDADOS: Buscar livros recomendados
+ 4 - LISTA DE DESEJOS: Busca livros nao lidos
+ 
+ 5 - SAIR
+
+ Digite um numero[1-5]: 
+ `)
+
+switch (resposta) {
+    case '1':
+        //chamar a função buscar livros. Que tá no arquivo dentro do controllers
+        buscarLivros()
+        break
+    case '2':
+        //chame a função ordenar.
+        listarLivrosOrdenados()
+        break
+    case '3':
+        //chame a função recomendados.
+        listaRecomendados()
+        break
+    case '4':
+        //chame a função lista de desejos
+        listarLivrosNaoLidos()
+    default:
+        console.log('Fim do algoritmo')
+        break
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Sistema de Gerenciamento de Livros
+
+## Projeto guiado realizado no curso Back-end na {reprograma}
+
+Essa semana foi a prova de fuego! Juntar todos os conhecimentos adquiridos até o momento em um único projeto! <br> 
+O projeto se trata de um **programa que gerencia livros**. 
+Trabalhamos com **array de objetos** que caracterizou cada livro por título, categoria, numero de paginas, status de leitura e se é recomendável. Para o desenvolvimento do programa foram necessários uso dos **métodos de iteração**: *filter(), sort()* e sua variação para **ordenar conforme característica específica**, *map()* entre outros artifícios como *functions, switch|case, readline-sync* etc. <br>
+Foi uma verdadeira aplicação do que aprendemos até agora no curso. Foi muito massa! Consegui fazer o exercício antes da aula de revisão da quarta-feira e to me sentindo muito computacional!
+<br>
+![](5SM.gif)
